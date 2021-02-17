8ef8931a8b138a70bcb46a6d87121fae1084800d
keiKunn <k.aragn03@gmail.com>
2021-02-17 10:28:48 +0900

    React課題②－２ answersのreducerに渡すinitialStateを修正

--- form-app/src/js/reducks/answers/reducers.js
+++ form-app/src/js/reducks/answers/reducers.js
@@ -3,7 +3,7 @@ import initialState from '../store/initialState'
 
 // 第一引数：state
 // 第二引数：actionがreturnした値
-export const AnswersReducer = (state = initialState.users, action) => {
+export const AnswersReducer = (state = initialState.answers, action) => {
   switch (action.type) {
     case Actions.QUESTIONS_PUSH_NEXT:
       return {
